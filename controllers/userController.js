5a5dd08e022a40a1f7d84be94dee7e40dbca2483
Zeptosec <armandasmatkevicius@gmail.com>
2022-11-24 21:35:33 +0200

    user login and reg

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
 import jwt from 'jsonwebtoken';
+import mysql from 'mysql2/promise';
+import  bcrypt from 'bcrypt';
+import validator from 'validator';
 
 const createToken = (id) => {
     return jwt.sign({ _id: id }, process.env.SECRET, { expiresIn: '6h' });
@@ -6,15 +9,24 @@ const createToken = (id) => {
 
 // login
 export const loginUser = async (req, res) => {
-    const { email, password } = req.body;
+    const { username, password } = req.body;
 
     try {
-        //const user = await User.login(email, password);
+        if(!username || !password){
+            throw Error("Fill in all fields");
+        }
+        const connection = await mysql.createConnection(process.env.DATABASE_URL);
+        const query = `SELECT * FROM Vartotojas WHERE Prisijungimo_vardas = '${username}'`;
+        const [rows] = await connection.query(query);
+        connection.end();
+        if(rows.length == 0){
+            throw Error("No such user");
+        }
 
-        //const token = createToken(user._id);
+        const token = createToken(rows[0].id_Vartotojas);
 
-        //res.status(200).json({ email, token });
-        res.status(200).json({ email, token: 123 });
+        
+        res.status(200).json({ username, token, tipas: rows[0].tipas });
     } catch (err) {
         res.status(400).json({ error: err.message });
     }
@@ -22,14 +34,38 @@ export const loginUser = async (req, res) => {
 
 // signup
 export const signupUser = async (req, res) => {
-    const { email, password } = req.body;
-
+    const { username, password } = req.body;
     try {
-        //const user = await User.signup(email, password);
+        if(!username || !password){
+            throw Error("Fill in all fields");
+        }
+        if (!validator.matches(username, "^[a-zA-Z0-9_\.\-]*$")) {
+            console.log('Username not valid');
+        }
+        if(!validator.isStrongPassword(password, { minLowercase: 0, minUppercase: 0, minNumbers: 0, minSymbols: 0 })){
+            throw Error("Password is weak");
+        }
+        // sql injection still is possible :( whatever
 
-        //const token = createToken(user._id);
+        const connection = await mysql.createConnection(process.env.DATABASE_URL);
+        const query = `SELECT * FROM Vartotojas WHERE Prisijungimo_vardas = '${username}'`;
+        const [rows] = await connection.query(query);
+        if(rows.length != 0){
+            throw Error("User exists");
+        }
 
-        //res.status(200).json({ email, token });
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        const rs = await connection.query(`INSERT INTO Vartotojas (Prisijungimo_vardas, Slaptazodis, tipas) VALUES ('${username}', '${hash}', 0)`);
+        const token = createToken(rs[0].insertId);
+        connection.end();
+
+        res.status(200).json({
+            username, 
+            tipas: 0,
+            token
+        });
     } catch (err) {
         res.status(400).json({ error: err.message });
     }
