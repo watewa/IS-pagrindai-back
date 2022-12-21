fcc784810459e5de49671efd984b733bcbc26845
Jonas <jonas.liubinavicius@gmail.com>
2022-12-21 12:20:37 +0200

    orederiai su prekemis

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,7 +3,6 @@ import mysql from 'mysql2/promise';
 export const deleteOrder = async (req, res) => {
     try {
         const { id } = req.params;
-        console.log(id);
         const connection = await mysql.createConnection(process.env.DATABASE_URL);
         const query = `SELECT * FROM Uzsakymas WHERE id_Uzsakymas='${id}'`;
         const [rows] = await connection.query(query);
@@ -25,8 +24,17 @@ export const deleteOrder = async (req, res) => {
 export const getAllOrders = async (req, res) => {
     try {
         const connection = await mysql.createConnection(process.env.DATABASE_URL);
-        const query = "SELECT * FROM Uzsakymas";
+        const query = "select nr, sukurimo_data	, busena, id_Uzsakymas, CONCAT(Vardas, ' ', Pavarde) as VarPav from Uzsakymas inner join Klientas on Uzsakymas.fk_Klientasid_Klientas=Klientas.id_Klientas";
+        
         const [rows] = await connection.query(query);
+        var subQuery = ``;
+        for (let i = 0; i<rows.length; i++){
+            subQuery = `SELECT * FROM Uzsakymo_preke
+                        INNER JOIN Preke ON Uzsakymo_preke.fk_Prekeid_Preke=Preke.id_Preke
+                        WHERE fk_Uzsakymasid_Uzsakymas='${rows[i].id_Uzsakymas}';`;
+            const [rows2] = await connection.query(subQuery);
+            rows[i].products = rows2;
+        }
         connection.end();
         res.status(200).json({
             orders: rows
@@ -74,7 +82,22 @@ export const updateOrder = async (req, res) => {
 
 export const createOrder = async (req, res) => {
     try {
-        const { data, busena, klientas } = req.body;
+        const { data, busena, klientas, prekes, kiekiai } = req.body;
+        for(let i = 0; i<prekes.length; i++){
+            if(prekes[i] == 'x' ){
+                prekes.splice(i, 1)
+                i--;
+            }
+        }
+        for(let i = 0; i<prekes.length; i++){
+            if(kiekiai[i] == 'x' ){
+                kiekiai.splice(i, 1)
+                i--;
+            }
+        }
+        if(kiekiai.length !== prekes.length){
+            throw Error("Netinkamas kiekių ir prekių skaičius");
+        }
         if (!data) {
             throw Error("Data negali būti tuščia");
         }
@@ -93,6 +116,16 @@ export const createOrder = async (req, res) => {
         )`;
         await connection.query(query);
         await connection.query(`UPDATE Uzsakymas SET nr = id_Uzsakymas`);
+        var id = await (await connection.query(`select max(id_Uzsakymas) as result from Uzsakymas`));
+        for (let i = 0; i<prekes.length; i++){
+            var query2 = `INSERT INTO Uzsakymo_preke (Kiekis, fk_Prekeid_Preke, fk_Uzsakymasid_Uzsakymas) VALUES (
+                '${kiekiai[i]}',
+                '${prekes[i]}',
+                '${id[0][0].result}'
+            )`;
+            await connection.query(query2);
+        }
+
         connection.end();
         res.status(200).json({ success: true });
     } catch (err) {
